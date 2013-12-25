51d33777805f6ca2ef0d1513a2b2b3d7810a5a96
Max Konev <max@arello-mobile.com>
2013-12-26 00:25:41 +0700

    unregister device request

--- Classes/PushNotificationManager.m
+++ Classes/PushNotificationManager.m
@@ -16,6 +16,7 @@
 #import "PWPushStatRequest.h"
 #import "PWGetNearestZoneRequest.h"
 #import "PWApplicationEventRequest.h"
+#import "PWUnregisterDeviceRequest.h"
 
 #import "PWLocationTracker.h"
 
@@ -394,6 +395,27 @@ static PushNotificationManager * instance = nil;
 		}
 	
 		[request release]; request = nil;
+	}
+}
+
+- (void) unregisterDevice {
+	@autoreleasepool {
+		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+			
+			PWUnregisterDeviceRequest *request = [PWUnregisterDeviceRequest new];
+			request.appId = appCode;
+			request.hwid = [self uniqueGlobalDeviceIdentifier];
+			
+			NSError *error = nil;
+			if ([[PWRequestManager sharedManager] sendRequest:request error:&error]) {
+				NSLog(@"Unregistered for push notifications");
+			} else {
+				NSLog(@"Unregistering for push notifications failed");
+			}
+			
+			[request release];
+			
+		});
 	}
 }
 
