6a5bb42608e58084d04e9d060e42e6e231e9a80b
Dmitry Malugin <diman@pushwoosh.com>
2016-09-29 11:57:05 +0600

    iOS10 Notification Service Extension example

---
+++ Samples/iPhone/NotificationService/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  NotificationService
+//
+//  Created by User on 29/09/16.
+//
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
---
+++ Samples/iPhone/NotificationService/NotificationService.m
@@ -0,0 +1,59 @@
+//
+//  NotificationService.m
+//  NotificationService
+//
+//  Created by User on 29/09/16.
+//
+//
+
+#import "NotificationService.h"
+
+@interface NotificationService ()
+
+@property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
+@property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
+
+@end
+
+@implementation NotificationService
+
+- (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+	self.contentHandler = contentHandler;
+	self.bestAttemptContent = [request.content mutableCopy];
+	NSString *attachmentUrlString = [request.content.userInfo objectForKey:@"attachment"];
+	if ([attachmentUrlString isKindOfClass:[NSString class]]) {
+		NSURL *url = [NSURL URLWithString:attachmentUrlString];
+		if (url) {
+			[[[NSURLSession sharedSession] downloadTaskWithURL:url completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+				if (!error) {
+					NSString *tempDict = NSTemporaryDirectory();
+					NSString *attachmentID = [[[NSUUID UUID] UUIDString] stringByAppendingString:[response.URL.absoluteString lastPathComponent]];
+					NSString *tempFilePath = [tempDict stringByAppendingPathComponent:attachmentID];
+					if ([[NSFileManager defaultManager] moveItemAtPath:location.path toPath:tempFilePath error:&error]) {
+						UNNotificationAttachment *attachment = [UNNotificationAttachment attachmentWithIdentifier:attachmentID URL:[NSURL fileURLWithPath:tempFilePath] options:nil error:&error];
+						if (!attachment) {
+							NSLog(@"Create attachment error: %@", error);
+						} else {
+							_bestAttemptContent.attachments = [_bestAttemptContent.attachments arrayByAddingObject:attachment];
+						}
+					} else {
+						NSLog(@"Move file error: %@", error);
+					}
+					
+				} else {
+					NSLog(@"Download file error: %@", error);
+				}
+				[[NSOperationQueue mainQueue] addOperationWithBlock:^{
+					self.contentHandler(self.bestAttemptContent);
+				}];
+			}] resume];
+		}
+	}
+}
+- (void)serviceExtensionTimeWillExpire {
+    // Called just before the extension will be terminated by the system.
+    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+    self.contentHandler(self.bestAttemptContent);
+}
+
+@end
