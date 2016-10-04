ee23238ee06f5326a72d1db0692be19a65140c89
Max Konev <max@arello-mobile.com>
2016-10-04 17:59:10 -0400

    Updated service to support URLs without extension

--- Samples/iPhone/NotificationService/NotificationService.m
+++ Samples/iPhone/NotificationService/NotificationService.m
@@ -21,34 +21,43 @@
 	self.contentHandler = contentHandler;
 	self.bestAttemptContent = [request.content mutableCopy];
 	NSString *attachmentUrlString = [request.content.userInfo objectForKey:@"attachment"];
-	if ([attachmentUrlString isKindOfClass:[NSString class]]) {
-		NSURL *url = [NSURL URLWithString:attachmentUrlString];
-		if (url) {
-			[[[NSURLSession sharedSession] downloadTaskWithURL:url completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-				if (!error) {
-					NSString *tempDict = NSTemporaryDirectory();
-					NSString *attachmentID = [[[NSUUID UUID] UUIDString] stringByAppendingString:[response.URL.absoluteString lastPathComponent]];
-					NSString *tempFilePath = [tempDict stringByAppendingPathComponent:attachmentID];
-					if ([[NSFileManager defaultManager] moveItemAtPath:location.path toPath:tempFilePath error:&error]) {
-						UNNotificationAttachment *attachment = [UNNotificationAttachment attachmentWithIdentifier:attachmentID URL:[NSURL fileURLWithPath:tempFilePath] options:nil error:&error];
-						if (!attachment) {
-							NSLog(@"Create attachment error: %@", error);
-						} else {
-							_bestAttemptContent.attachments = [_bestAttemptContent.attachments arrayByAddingObject:attachment];
-						}
-					} else {
-						NSLog(@"Move file error: %@", error);
-					}
-					
+	
+	if (![attachmentUrlString isKindOfClass:[NSString class]])
+		return;
+
+	NSURL *url = [NSURL URLWithString:attachmentUrlString];
+	if (!url)
+		return;
+
+	[[[NSURLSession sharedSession] downloadTaskWithURL:url completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+		if (!error) {
+			NSString *tempDict = NSTemporaryDirectory();
+			NSString *attachmentID = [[[NSUUID UUID] UUIDString] stringByAppendingString:[response.URL.absoluteString lastPathComponent]];
+			
+			if(response.suggestedFilename)
+				attachmentID = [[[NSUUID UUID] UUIDString] stringByAppendingString:response.suggestedFilename];
+
+			NSString *tempFilePath = [tempDict stringByAppendingPathComponent:attachmentID];
+
+			if ([[NSFileManager defaultManager] moveItemAtPath:location.path toPath:tempFilePath error:&error]) {
+				UNNotificationAttachment *attachment = [UNNotificationAttachment attachmentWithIdentifier:attachmentID URL:[NSURL fileURLWithPath:tempFilePath] options:nil error:&error];
+
+				if (!attachment) {
+					NSLog(@"Create attachment error: %@", error);
 				} else {
-					NSLog(@"Download file error: %@", error);
+					_bestAttemptContent.attachments = [_bestAttemptContent.attachments arrayByAddingObject:attachment];
 				}
-				[[NSOperationQueue mainQueue] addOperationWithBlock:^{
-					self.contentHandler(self.bestAttemptContent);
-				}];
-			}] resume];
+			} else {
+				NSLog(@"Move file error: %@", error);
+			}
+		} else {
+			NSLog(@"Download file error: %@", error);
 		}
-	}
+
+		[[NSOperationQueue mainQueue] addOperationWithBlock:^{
+			self.contentHandler(self.bestAttemptContent);
+		}];
+	}] resume];
 }
 - (void)serviceExtensionTimeWillExpire {
     // Called just before the extension will be terminated by the system.
