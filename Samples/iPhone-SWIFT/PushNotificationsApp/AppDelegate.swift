41d35a6d2f739b13be778994155dccdae6facf1a
Dmitry Malugin <diman@pushwoosh.com>
2016-11-11 19:24:22 +0600

    Samples update for XCode 8 and Swift 3.0

--- Samples/iPhone-SWIFT/PushNotificationsApp/AppDelegate.swift
+++ Samples/iPhone-SWIFT/PushNotificationsApp/AppDelegate.swift
@@ -14,52 +14,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PushNotificationDelegate 
 	var window: UIWindow?
 
 
-	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-		PushNotificationManager.pushManager().delegate = self
-		PushNotificationManager.pushManager().handlePushReceived(launchOptions)
-		PushNotificationManager.pushManager().sendAppOpen()
-		PushNotificationManager.pushManager().registerForPushNotifications()
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+		PushNotificationManager.push().delegate = self
+		PushNotificationManager.push().handlePushReceived(launchOptions)
+		PushNotificationManager.push().sendAppOpen()
+		PushNotificationManager.push().registerForPushNotifications()
 		return true
 	}
 	
-	func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
-		PushNotificationManager.pushManager().handlePushRegistration(deviceToken)
+	func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+		PushNotificationManager.push().handlePushRegistration(deviceToken as Data!)
 	}
 	
-	func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
-		PushNotificationManager.pushManager().handlePushRegistrationFailure(error)
+	func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+		PushNotificationManager.push().handlePushRegistrationFailure(error)
 	}
 	
-	func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
-		PushNotificationManager.pushManager().handlePushReceived(userInfo)
+	func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+		PushNotificationManager.push().handlePushReceived(userInfo)
 	}
 	
-	func onPushAccepted(pushManager: PushNotificationManager!, withNotification pushNotification: [NSObject : AnyObject]!, onStart: Bool) {
-		print("Push notification accepted: \(pushNotification)");
+	func onPushAccepted(_ pushManager: PushNotificationManager!, withNotification pushNotification: [AnyHashable : Any]!, onStart: Bool) {
+		print("Push notification accepted: \(pushNotification)")
 	}
-
-	func applicationWillResignActive(application: UIApplication) {
-		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-		// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-	}
-
-	func applicationDidEnterBackground(application: UIApplication) {
-		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-	}
-
-	func applicationWillEnterForeground(application: UIApplication) {
-		// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-	}
-
-	func applicationDidBecomeActive(application: UIApplication) {
-		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-	}
-
-	func applicationWillTerminate(application: UIApplication) {
-		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-	}
-
-
 }
 
